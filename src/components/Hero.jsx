23dd1de175042d8626fa849941efecbc607afa78
rahmate2003 <w.rahmad26@gmail.com>
2025-06-01 12:09:41 +0800

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -52,7 +52,7 @@ export default function Hero() {
                                 <path fillRule="evenodd" d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" clipRule="evenodd" />
                             </svg>
                         </a>
-                        <a
+                        {/* <a
                             href="https://instagram.com/matterizz_"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -62,7 +62,7 @@ export default function Hero() {
                             <svg className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24">
                                 <path d="M7.75 2A5.75 5.75 0 002 7.75v8.5A5.75 5.75 0 007.75 22h8.5A5.75 5.75 0 0022 16.25v-8.5A5.75 5.75 0 0016.25 2h-8.5zm0 1.5h8.5a4.25 4.25 0 014.25 4.25v8.5a4.25 4.25 0 01-4.25 4.25h-8.5a4.25 4.25 0 01-4.25-4.25v-8.5a4.25 4.25 0 014.25-4.25zm4.25 3.75a4 4 0 100 8 4 4 0 000-8zm0 1.5a2.5 2.5 0 110 5 2.5 2.5 0 010-5zm5.25-.25a.75.75 0 10-1.5 0 .75.75 0 001.5 0z" />
                             </svg>
-                        </a>
+                        </a> */}
                     </div>
 
                     {/* Buttons with enhanced styling */}
